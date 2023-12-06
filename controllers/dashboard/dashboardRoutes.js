9086f5f663147b6fc728b7bec927319485c11b7f
N-lara <n_lara01@yahoo.com>
2023-12-06 14:33:36 -0600

    working on handlebars for dashboard

--- controllers/dashboard/dashboardRoutes.js
+++ controllers/dashboard/dashboardRoutes.js
@@ -1,36 +1,31 @@
 const router = require("express").Router();
-const { Charactersheet, Focus, Race, Origin } = require("../../models");
+const { Charactersheet, Focus, Race, Origin, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// //gets all characters for user signed in TO SEPERATE PAGE
-// router.get('/characters', withAuth, async (req, res) => {
-//     try {
-//       //gets a users created characters
-//       console.log("get request to charactersheet");
-//       const charactersheetData = await Charactersheet.findAll({
-//         where: {user_id: req.session.user_id},
-//         include: [{ model: Origin }, { model: Race }, { model: Focus }],
-//       });
-//       const charactersheets = charactersheetData.map((charactersheet) => charactersheet.get({ plain: true }));
-//       console.log(charactersheets);
-//       res.status(200).json(charactersheetData).render("testchar", { charactersheets });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-// });
+//gets all characters for user signed in TO SEPERATE PAGE
+router.get('/', withAuth, async (req, res) => {
+    try {
+      //gets a users created characters
+      console.log("get request to charactersheet");
+      const charactersheetData = await Charactersheet.findAll({
+        where: {user_id: req.session.user_id},
+        include: [{ model: Origin }, { model: Race }, { model: Focus }, {model: User}],
+        exclude:["password"]
+      });
+      const charactersheets = charactersheetData.map((charactersheet) => charactersheet.get({ plain: true }));
+      console.log(charactersheets);
+      res.status(200).json(charactersheetData).render("testchar", { 
+           charactersheets,
+           loggedin: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
       //gets info from tables for dropdown menu
 router.get('/new-character', withAuth, async (req, res) => {
     try {
-        //start for getting characters DELETE IF TRANSFER TO SEPERATE PAGE\/\/\/
-        console.log("get request to charactersheet");
-        const charactersheetData = await Charactersheet.findAll({
-            where: {user_id: req.session.user_id},
-            include: [{ model: Origin }, { model: Race }, { model: Focus }],
-        });
-        const charactersheets = charactersheetData.map((charactersheet) => charactersheet.get({ plain: true }));
-        console.log(charactersheets);
-        //end getting characters DELETE IF TRANSFER TO SEPERATE PAGE ^^^
         const focusData = await Focus.findAll();
         const focuses = focusData.map((focus) => focus.get({ plain: true }));
 
@@ -41,11 +36,11 @@ router.get('/new-character', withAuth, async (req, res) => {
         const races = raceData.map((race) => race.get({ plain: true }));
      
         res.status(200).render("testchar", {
-        charactersheets,//for getting characters DELETE IF TRANSFER TO SEPERATE PAGE<-
         races,
         origins,
-        focuses
-        });
+        focuses,
+        loggedin: req.session.logged_in
+        }, );
     } catch (err) {
       res.status(500).json(err);
     }
